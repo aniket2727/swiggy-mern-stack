a72eb67a512dac2564e68a119074b9000bf6636d
aniket2727 <aniketkadam5950@gmail.com>
2025-01-08 16:39:21 +0530

    Add client folder to the repository

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import {BrowserRouter,Route,Routes}   from 'react-router-dom';
+
+function App() {
+  return (
+      <div>
+          <BrowserRouter>
+              <Routes>
+                <Route/>
+              </Routes>
+          </BrowserRouter>
+      </div>
+  );
+}
+
+export default App;
